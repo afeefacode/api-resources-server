ddea558a51910d58fb047f56e808090591ceaff1
Kaken Bok <bok@kakenbok.de>
2021-07-20 16:05:20 +0200

    set version: v0.0.58

---
+++ src/Validator/Validators/SelectValidator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Afeefa\ApiResources\Validator\Validators;
+
+use Afeefa\ApiResources\Validator\Rule\RuleBag;
+use Afeefa\ApiResources\Validator\Validator;
+
+class SelectValidator extends Validator
+{
+    public static string $type = 'Afeefa.SelectValidator';
+
+    public function filled(): SelectValidator
+    {
+        return $this->param('filled', true);
+    }
+
+    protected function rules(RuleBag $rules): void
+    {
+        $rules->add('filled')
+            ->message('{{ fieldLabel }} sollte eine Auswahl enthalten.')
+            ->validate(function (?string $value, bool $filled) {
+                if ($filled && !$value) {
+                    return false;
+                }
+                return true;
+            });
+    }
+}
