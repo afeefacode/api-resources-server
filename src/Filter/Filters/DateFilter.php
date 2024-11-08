dedc831a3f5e2e979bbfdc9e23fb9fafb62083df
Kaken Bok <bok@kakenbok.de>
2024-11-08 20:46:24 +0100

    set version: v0.2.5

---
+++ src/Filter/Filters/DateFilter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Afeefa\ApiResources\Filter\Filters;
+
+use Afeefa\ApiResources\Filter\Filter;
+
+class DateFilter extends Filter
+{
+    protected static string $type = 'Afeefa.DateFilter';
+}
