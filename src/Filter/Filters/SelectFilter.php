2d6957f94df8195f9045d89500d0120b1b3c8171
Kaken Bok <bok@kakenbok.de>
2024-11-14 15:59:54 +0100

    set version: v0.2.7

--- src/Filter/Filters/SelectFilter.php
+++ src/Filter/Filters/SelectFilter.php
@@ -7,4 +7,23 @@ use Afeefa\ApiResources\Filter\Filter;
 class SelectFilter extends Filter
 {
     protected static string $type = 'Afeefa.SelectFilter';
+
+    private bool $multiple = false;
+
+    public function multiple(): static
+    {
+        $this->multiple = true;
+        return $this;
+    }
+
+    public function toSchemaJson(): array
+    {
+        $json = parent::toSchemaJson();
+
+        if ($this->multiple) {
+            $json['multiple'] = true;
+        }
+
+        return $json;
+    }
 }
